c2d3ff222cd1b1f91e79ef25cc2b8fba126fc43d
Krishna D N <krishnadn94@gmail.com>
2023-01-23 00:10:43 +0530

    Added tests

--- tests/test_speech_kitchen.py
+++ tests/test_speech_kitchen.py
@@ -1,1 +1,10 @@
-from speech_kitchen import speech_kitchen
+import numpy as np
+import soundfile as sf
+from src.speech_kitchen import downsampler
+import os
+
+def test_downsampler(sr=16000):
+    data = np.random.rand(32000)
+    sf.write('src.wav', data, 22000)
+    process = downsampler('src.wav','dest.wav',16000)
+    assert os.path.exists('dest.wav')==True
